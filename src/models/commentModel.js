7530b01deb7e414ababda65d9abe7fa9c7f70b92
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2025-04-30 21:48:25 -0300

    feat: implement CRUD operations for comments in controller and model

--- src/models/commentModel.js
+++ src/models/commentModel.js
@@ -1,6 +1,7 @@
 import prisma from "../../prisma/prisma.js";
 
 class CommentModel {
+  // Criar comentário
   async create(data) {
     return await prisma.comment.create({
       data,
@@ -8,6 +9,22 @@ class CommentModel {
     });
   }
 
+  // Buscar todos os comentários
+  async getAll() {
+    return await prisma.comment.findMany({
+      include: { user: true }
+    });
+  }
+
+  // Buscar comentário por ID
+  async findById(id) {
+    return await prisma.comment.findUnique({
+      where: { id },
+      include: { user: true }
+    });
+  }
+
+  // Buscar comentários por produto
   async getByProduct(produtoId) {
     return await prisma.comment.findMany({
       where: { produtoId },
@@ -15,6 +32,15 @@ class CommentModel {
     });
   }
 
+  // Atualizar comentário
+  async update(id, data) {
+    return await prisma.comment.update({
+      where: { id },
+      data
+    });
+  }
+
+  // Deletar comentário
   async delete(id) {
     await prisma.comment.delete({
       where: { id }
